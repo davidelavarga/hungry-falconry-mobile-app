77f4a5e92813a430182298f06235eaf253f84e08
David de la Varga <davidelavarga@gmail.com>
2020-04-25 16:37:32 +0200

    refresh after delete a schedule

--- lib/schedulelist/schedule_list.dart
+++ lib/schedulelist/schedule_list.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:using_listview/api_requests/schedule.dart';
 import 'package:using_listview/feederlist/modal/feeder.dart';
 import 'package:using_listview/schedulelist/modal/schedule.dart';
-import 'package:using_listview/schedulelist/schedule_list_item.dart';
 import 'package:intl/intl.dart';
 
 int scheduleLength = 0;
@@ -21,15 +20,71 @@ class _ScheduleState extends State<ScheduleStateful> {
   _ScheduleState({@required this.feederModel});
   @override
   Widget build(BuildContext context) {
-    ScheduleList scheduleList = ScheduleList(feederModel: feederModel);
     return Scaffold(
       appBar: AppBar(
         title: Text(feederModel.nameByUser + ' Schedules'),
       ),
-      body: scheduleList,
+      body: Container(
+        child: FutureBuilder(
+          future: fetchSchedulesByFeeder(feederModel.id),
+          // ignore: missing_return
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Text("there is no connection");
+
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return Center(child: new CircularProgressIndicator());
+
+              case ConnectionState.done:
+                scheduleLength = 0;
+                if (snapshot.data != null) {
+                  List<ScheduleModel> schedules =
+                      new List(snapshot.data["results"].length);
+                  for (var i = 0; i < snapshot.data["results"].length; i++) {
+                    schedules[i] =
+                        ScheduleModel.fromJson(snapshot.data["results"][i]);
+
+                    if (!schedules[i].done) scheduleLength++;
+                  }
+
+                  return ListView.builder(
+                      itemExtent: 90,
+                      itemCount: schedules
+                          .length, // getting map length you can use keyList.length too
+                      itemBuilder: (BuildContext context, int index) {
+                        ScheduleModel currentScheduleItem = schedules[index];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.brown.shade800,
+                            child: Text(""),
+                          ),
+                          title:
+                              Text(currentScheduleItem.timestamp.toUpperCase()),
+                          trailing: Icon(
+                            currentScheduleItem.done
+                                ? Icons.done
+                                : Icons.access_time,
+                            color: currentScheduleItem.done
+                                ? Colors.green
+                                : Colors.black26,
+                          ),
+                          onLongPress: () => {
+                            _alertDeleteSchedule(
+                                context, feederModel.id, currentScheduleItem.id)
+                          },
+                        );
+                      });
+                }
+                return Text("No data was loaded from schedules");
+            } //end switch
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          if (scheduleLength >= feederModel.maxPortions){
+          if (scheduleLength >= feederModel.maxPortions) {
             await _alertNoMoreSchedules();
             return;
           }
@@ -78,6 +133,39 @@ class _ScheduleState extends State<ScheduleStateful> {
     );
   }
 
+  void _alertDeleteSchedule(BuildContext context, int feeder, int schedule) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Delete", style: TextStyle(color: Colors.black)),
+          content: new Text("Are you sure?"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Delete"),
+              textColor: Colors.red,
+              onPressed: () async {
+                await deleteSchedule(feeder, schedule);
+                _refreshSchedules();
+                Navigator.of(context).pop();
+              },
+            ),
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void _refreshSchedules() {
     setState(() {
       feederModel = this.feederModel;
@@ -102,54 +190,6 @@ class _ScheduleState extends State<ScheduleStateful> {
     return showTimePicker(
       context: context,
       initialTime: TimeOfDay(hour: now.hour, minute: now.minute),
-    );
-  }
-}
-
-class ScheduleList extends StatelessWidget {
-  final FeederModel feederModel;
-
-  ScheduleList({@required this.feederModel});
-
-  @override
-  Widget build(BuildContext ctxt) {
-    return Container(
-      child: FutureBuilder(
-        future: fetchSchedulesByFeeder(feederModel.id),
-        // ignore: missing_return
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return Text("there is no connection");
-
-            case ConnectionState.active:
-            case ConnectionState.waiting:
-              return Center(child: new CircularProgressIndicator());
-
-            case ConnectionState.done:
-              scheduleLength = 0;
-              if (snapshot.data != null) {
-                List<ScheduleModel> schedules =
-                    new List(snapshot.data["results"].length);
-                for (var i = 0; i < snapshot.data["results"].length; i++) {
-                  schedules[i] =
-                      ScheduleModel.fromJson(snapshot.data["results"][i]);
-
-                  if (!schedules[i].done) scheduleLength++;
-                }
-
-                return ListView.builder(
-                    itemExtent: 90,
-                    itemCount: schedules
-                        .length, // getting map length you can use keyList.length too
-                    itemBuilder: (BuildContext context, int index) {
-                      return ScheduleListItem(schedules[index], feederModel);
-                    });
-              }
-              return Text("No data was loaded from schedules");
-          } //end switch
-        },
-      ),
     );
   }
 }
